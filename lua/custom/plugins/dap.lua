7743dadc40342132e77ce249545d17d074a12282
Joey Chang <tsungyc@uci.edu>
2024-05-24 01:57:25 -0700

    finish dap?

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -5,16 +5,42 @@ return {
       "leoluz/nvim-dap-go",
       "rcarriga/nvim-dap-ui",
       "nvim-neotest/nvim-nio",
+      "theHamsta/nvim-dap-virtual-text",
+      "williamboman/mason.nvim",
     },
     config = function()
       local dap = require "dap"
       local ui = require "dapui"
 
-      ui.setup()
-
+      require("dapui").setup()
       require("dap-go").setup()
+      require("nvim-dap-virtual-text").setup {
+        display_callback = function(variable)
+          local name = string.lower(variable.name)
+          if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+            return "*****"
+          end
 
-      vim.keymap.set("n", "<leader>b", require("dap").toggle_breakpoint)
+          if #variable.value > 15 then
+            return " " .. string.sub(variable.value, 1, 15) .. "... "
+          end
+          return " " .. variable.value
+        end,
+      }
+
+      vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
+
+      vim.keymap.set("n", "<F1>", dap.continue)
+      vim.keymap.set("n", "<F2>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<F6>", dap.restart)
+
+      vim.keymap.set("n", "<leader>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
 
       dap.listeners.before.attach.dapui_config = function()
         ui.open()
