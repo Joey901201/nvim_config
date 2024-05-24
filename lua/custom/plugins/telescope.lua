e8b46262ed8660abbc46ec020da8a458e874d2d6
Joey Chang <tsungyc@uci.edu>
2024-05-23 22:10:14 -0700

    bit improve, bug need fix

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,12 +1,27 @@
 return {
-  {"nvim-lua/plenary.nvim"},
-  {"nvim-telescope/telescope.nvim", branch = '0.1.x',
+  { "nvim-lua/plenary.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
     config = function()
-      local builtin =require("telescope.builtin")
+      require("telescope").setup {
+        extensions = {
+          fzf = {},
+        },
+      }
 
-      vim.keymap.set("n","<space>fd", function()
-        builtin.find_files()
+      pcall(require("telescope").load_extension, "fzf")
+
+      local builtin = require "telescope.builtin"
+
+      vim.keymap.set("n", "<leader>fd", builtin.find_files)
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files { cwd = vim.fn.stdpath "config" }
       end)
-
-    end }
+    end,
+  },
 }
